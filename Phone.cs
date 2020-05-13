c922756db5b6a4acc4f8a0f096a4b4774479284f
gkennedy982 <gkennedy982@gmail.com>
2020-05-13 10:31:22 +0100

    Q5 Completed

---
+++ Phone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GavanKennedy_S00180333
+{
+    public class Phone
+    {
+        public int PhoneID { get; set; }
+        public string Name { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OsImage { get; set; }
+        public string PhoneImage { get; set; }
+        public double Price { get; set; }
+
+        public void IncreasePrice()
+        {
+
+        }
+    }
+    public class PhoneData:DbContext
+    {
+        public PhoneData() : base("ExamData") { }
+        public DbSet<Phone> Phones { get; set; }
+    }
+}
